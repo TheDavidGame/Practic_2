f0f2407bef697b8fdbaad169ae38b448d827f0fe
TheDavidGame <vlad.billI@bk.ru>
2020-09-06 14:58:36 +0300

    start prog
    go

--- src/Praric_2/first_class.java
+++ src/Praric_2/first_class.java
@@ -1,4 +1,9 @@
 package Praric_2;
-
+import java.util.Scanner;
 public class first_class {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
 }
